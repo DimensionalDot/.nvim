70c7ca344da2ef8a046682b6a8be0afb43489875
DimensionalDot <dimensionaldot@gmail.com>
2023-06-26 01:13:44 -0400

    feat: moved statusline to after and improved colors (inc. transparency)

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,5 +1,18 @@
 vim.cmd('set termguicolors')
-vim.cmd('colorscheme rose-pine-moon')
--- vim.cmd('colorscheme gruvbox')
+
+require('rose-pine').setup({
+    -- groups = {
+    --     background = "none",
+    -- }
+})
+
+vim.cmd.colorscheme("rose-pine-moon")
+-- vim.cmd.colorscheme("gruvbox")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+-- still highlights line number if cursorline = true
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 
 -- print("after.plugin")
--- lua/dimensional/init.lua
+++ lua/dimensional/init.lua
@@ -1,4 +1,3 @@
 require("dimensional.set")
 require("dimensional.remap")
 require("dimensional.lsp")
-require("dimensional.statusline")
