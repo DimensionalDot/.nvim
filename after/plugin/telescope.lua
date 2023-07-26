1d9ea84465af4569a16d013f266dbb56ae8baf08
DimensionalDot <dimensionaldot@gmail.com>
2023-07-25 22:42:23 -0400

    feat: split telescope grep and find_files function back into two remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,4 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>tf", function()
-    local status, err = pcall(builtin.git_files)
-    -- fallback to files in current directory if not in git project
-    -- currently if git repo has no commits file finder will be empty...
-    if not status and err:find("not a git directory", 1, true) then
-        builtin.find_files()
-    end
-end)
+vim.keymap.set("n", "<leader>tf", builtin.find_files)
+vim.keymap.set("n", "<leader>tv", builtin.git_files)
 vim.keymap.set("n", "<leader>tg", builtin.live_grep)
