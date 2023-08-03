59310744f121a02d6c473af5f9d2cfd9a27e3a51
DimensionalDot <dimensionaldot@gmail.com>
2023-08-03 17:34:37 -0400

    feat: jumping between lsp diagnostics

--- lua/dimensional/lsp.lua
+++ lua/dimensional/lsp.lua
@@ -2,6 +2,8 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 
 on_attach = function()
     vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, {buffer=0})
+    vim.keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, {buffer=0})
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
     vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, {buffer=0})
