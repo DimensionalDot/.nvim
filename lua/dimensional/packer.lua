f277372c92d3908ac81a052070457107c0d7f4c3
DimensionalDot <dimensionaldot@gmail.com>
2024-03-22 12:38:47 -0400

    feat: commenting now working within jsx/tsx

--- lua/dimensional/packer.lua
+++ lua/dimensional/packer.lua
@@ -51,6 +51,17 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
+    use {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        config = function()
+            require('ts_context_commentstring').setup {
+                enable_autocmd = false,
+            }
+            require('Comment').setup {
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+            }
+        end
+    }
 
     -- undotree
     use 'mbbill/undotree'
