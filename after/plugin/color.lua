0828a5c4530a600255e7099e2c3f867bdb33c6e7
DimensionalDot <dimensionaldot@gmail.com>
2025-01-26 11:16:23 -0500

    feat: added color config for transparent terminal background

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -6,13 +6,28 @@ require('rose-pine').setup({
     -- }
 })
 
-vim.cmd.colorscheme("rose-pine-moon")
--- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("rose-pine-moon")
+vim.cmd.colorscheme("gruvbox")
+
+-- TRANSPARENT CONFIG:
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 -- still highlights line number if cursorline = true
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { link = "NormalFloat" })
 
--- print("after.plugin")
+-- setup lsp borders
+local border_type = "rounded"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = border_type
+  }
+)
+
+vim.diagnostic.config{
+  float = { border = border_type }
+}
+
