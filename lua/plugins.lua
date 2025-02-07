30095a65183694ee163b9c65ca7d7c95332b2a79
DimensionalDot <dimensionaldot@gmail.com>
2025-02-07 11:20:29 -0500

    ref: plugin spec cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,61 +1,56 @@
 return {
     -- colors
-    'gruvbox-community/gruvbox',
-    { 'rose-pine/neovim', name = 'rose-pine' },
-    'folke/tokyonight.nvim',
-    'Mofiqul/dracula.nvim',
+    "gruvbox-community/gruvbox",
+    { "rose-pine/neovim",              name = "rose-pine" },
+    "folke/tokyonight.nvim",
+    "Mofiqul/dracula.nvim",
 
     -- lualine
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
-    },
+    "nvim-lualine/lualine.nvim",
 
     -- telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim', "nvim-tree/nvim-web-devicons"} }
-    },
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
+    { "nvim-lua/plenary.nvim",         lazy = true },
+
+    -- icons
+    { "nvim-tree/nvim-web-devicons",   lazy = true },
 
     -- language awareness
     {
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-treesitter/nvim-treesitter-context',
-        'nvim-treesitter/playground',
-        build = ':TSUpdate'
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter-context",
+        "nvim-treesitter/playground",
+        build = ":TSUpdate"
     },
-    'neovim/nvim-lspconfig',
-    'onsails/lspkind.nvim',
+    "neovim/nvim-lspconfig",
+    "onsails/lspkind.nvim",
 
     -- cmp
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
 
     -- comments
-    'numToStr/Comment.nvim',
+    "numToStr/Comment.nvim",
 
     {
-        'JoosepAlviste/nvim-ts-context-commentstring',
+        "JoosepAlviste/nvim-ts-context-commentstring",
         opts = { enable_autocmd = false },
         config = function()
-            require('Comment').setup {
+            require("Comment").setup {
                 pre_hook = require(
-                    'ts_context_commentstring.integrations.comment_nvim'
+                    "ts_context_commentstring.integrations.comment_nvim"
                 ).create_pre_hook(),
             }
         end
     },
 
     -- undotree
-    'mbbill/undotree',
+    "mbbill/undotree",
 
     -- git
-    'tpope/vim-fugitive',
-
-    'lervag/vimtex',
+    "tpope/vim-fugitive",
 }
