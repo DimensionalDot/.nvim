57c83e55145cd945e6612981fccf509a808500ec
DimensionalDot <dimensionaldot@gmail.com>
2025-02-08 11:18:15 -0500

    feat: telescope uses git_files when appropriate (& updated lazy-lock)

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,14 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>tf", function() 
-    builtin.find_files({ hidden = true})
+vim.keymap.set("n", "<leader>tf", function()
+    vim.fn.system("git rev-parse --is-inside-work-tree") -- maybe cache this?
+    local in_git = vim.v.shell_error == 0
+
+    if in_git then
+        builtin.git_files()
+    else
+        builtin.find_files({ hidden = true })
+    end
 end)
-vim.keymap.set("n", "<leader>tv", builtin.git_files)
+vim.keymap.set("n", "<leader>ta", builtin.find_files)
 vim.keymap.set("n", "<leader>tg", builtin.live_grep)
 vim.keymap.set("n", "<leader>te", builtin.diagnostics)
